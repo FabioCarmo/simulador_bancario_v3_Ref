95432af10e9b2ebf2ab228fcef31444451efe5e4
fabio <fcarmouk13@gmail.com>
2025-11-06 12:07:10 -0300

    Ref: Metodos foram refatorados. Novo metodo cursor

--- models/conexaodb.py
+++ models/conexaodb.py
@@ -9,14 +9,17 @@ class Conexao():
         self._PATH = Path(__file__).parent.resolve() # Diretorio do projeto
         self._enderecodb = str(self._PATH / "bancodasnotas.sqlite3")
         self._conectado = False
+        self._cursordb = None
 
+    # Conectar com a base de dados sqlite3
     def conectar(self):
         try:
             self._conexaodb = sqlite3.connect(self._enderecodb) # conecta com db
             self._conectado = True
+            self._cursordb = self._conexaodb.cursor()
             return self._conexaodb
-        except sqlite3.Error as exc:
-            return exc
+        except:
+            return False
 
     # Encerra a conexao com a base de dados
     def fechar(self):
@@ -26,9 +29,13 @@ class Conexao():
             self._conexaodb = None
 
 
-    # retorna metodo cursor
+    # retorna atributo com metodo cursor
     @property   
     def cursor(self):
-        if (self._conectado): 
-            return self._conexaodb.cursor()
+        return self._cursordb
+        
+    # retorna atributo conectado
+    @property   
+    def dbconectado(self):
+        return self._conectado
         
