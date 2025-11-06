f2261a266f36f3edc752d38c080d9e37f538ce2a
fabio <fcarmouk13@gmail.com>
2025-11-06 10:51:04 -0300

    Fix: Alterado modulo e funcao menu excluido

--- views/opcoes.py
+++ views/opcoes.py
@@ -1,32 +1,10 @@
 
 # Importar modulo de identacao
-import textwrap
 from src.cliente import Cliente, PessoaFisica, Conta, ContaCorrente, Historico, Transacao, Saque, Deposito
 from models.dominio import DbBanco
 from src.validarcpf import filtrarcpf
 
 db = DbBanco()
-
-def menu(clientes, contas):
-
-    opcoes_menu_login = ['[1]\tDepositar', '[2]\tSacar', '[3]\tExtrato', '[4]\tNovo usuario', '[9]\tExcluir meus dados', '[0]\tSair\n=>', '[1]\tNova Conta', '[2]\tLogin', '[0]\tSair\n=>']
-    menu = ""
-
-    if not clientes:
-        opcoes_validas = opcoes_menu_login[6:]
-
-    else:
-        opcoes_validas = opcoes_menu_login[:6]
-
-        # Se o usuario tiver conta apaga a posicao de novo usuario da lista de opcoes
-        if contas:
-            del opcoes_validas[3]
-
-    for valor in opcoes_validas:
-        menu += valor + '\n'
-
-    return input(textwrap.dedent(menu)).strip()
-
 
 def filtrar_cliente(cpf):
     cpf = (cpf,)
