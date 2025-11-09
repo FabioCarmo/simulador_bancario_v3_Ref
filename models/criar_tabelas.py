fe5bd47e780d7392dc60cc5c5a41ae5043b3a151
fabio <fcarmouk13@gmail.com>
2025-11-09 16:48:15 -0300

    Fix: Alterado tipo de coluna da tabela contas

--- models/criar_tabelas.py
+++ models/criar_tabelas.py
@@ -9,7 +9,7 @@ def criar_tabela():
 
     try:
         cursor.executescript("CREATE TABLE clientes (ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, nome VARCHAR(50) NOT NULL, cpf VARCHAR(15) NOT NULL UNIQUE, data_nascimento DATE NOT NULL, endereco VARCHAR(100) NOT NULL); \
-        CREATE TABLE contas (ID INTEGER NOT NULL, cpf VARCHAR NOT NULL UNIQUE, agencia INTEGER(4) NOT NULL, conta INTEGER(5) NOT NULL); \
+        CREATE TABLE contas (ID INTEGER NOT NULL, cpf VARCHAR NOT NULL UNIQUE, agencia VARCHAR(4) NOT NULL, conta INTEGER(5) NOT NULL); \
         CREATE TABLE saldo (ID INTEGER NOT NULL, saldo FLOAT(12)  NOT NULL DEFAULT 0.00); \
         CREATE TABLE transacao(ID INTEGER NOT NULL, tipo VARCHAR(10), valor FLOAT(12) NOT NULL, data DATE);")
         conexaodb.commit()
